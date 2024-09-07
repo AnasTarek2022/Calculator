cbf1da57c31e9e21f58d47f2c0db6f5988581964
AnasTarek2022 <118046685+AnasTarek2022@users.noreply.github.com>
2024-09-07 14:37:51 +0300

    add the project

---
+++ calc/calc.js
@@ -0,0 +1,23 @@
+//window.alert('hello dev')
+//function writetext ()
+//{ document.getElementById('text1').innerHTML='This is my first java script';}
+
+function getNum(num)
+{
+document.getElementById('result').innerHTML=num;
+}
+
+
+function sum(n1,n2) {
+return n1+n2;
+
+}
+function min(n1,n2){
+
+    return n1-n2
+}
+function eqaul(){
+
+    sum(n1,n2)
+
+}
